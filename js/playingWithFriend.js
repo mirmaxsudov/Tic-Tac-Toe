527202ada08c356d2a9f4037241648c34438e8e3
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2024-11-24 18:56:00 +0500

    added 'playing with friends'

---
+++ js/playingWithFriend.js
@@ -0,0 +1,160 @@
+const backIcon = document.querySelector(".back-icon");
+const backBtn = document.querySelector(".back-btn");
+const pauseIcon = document.querySelector(".pause-icon");
+const resumeBtn = document.querySelector(".resume-btn");
+
+const pauseModal = document.querySelector(".pause-modal");
+const overlayPause = document.querySelector(".overlay-pause");
+
+const oImageLink = "../images/O.svg";
+const xImageLink = "../images/X.svg";
+
+const headerFooterImgLink = document.querySelector(".header__footer-img");
+const headerFooterText = document.querySelector(".header__footer-text");
+
+const ticTacToe = document.querySelector(".tic-tac-toe");
+
+let TURN = "X";
+let isGameOver = false;
+
+window.onload = () => {
+  initializeGame();
+  attachNavigationListeners();
+  attachPauseResumeListeners();
+  attachTicTacToeEventListeners();
+};
+
+function initializeGame() {
+  updateHeaderFooter("Player 1 Turn", xImageLink);
+}
+
+function toggleTurn() {
+  TURN = TURN === "X" ? "O" : "X";
+}
+
+function updateHeaderFooter(text, imgSrc) {
+  headerFooterText.textContent = text;
+  headerFooterImgLink.src = imgSrc;
+}
+
+function ticTacToeItemHandler(ticTacToeItem) {
+  if (isGameOver) return;
+
+  const isUsed = ticTacToeItem.getAttribute("data-used") !== "NONE";
+
+  if (isUsed) {
+    alert("Already used");
+    return;
+  }
+
+  if (TURN === "X") {
+    turnX(ticTacToeItem);
+    if (checkWin("X")) {
+      endGame("X");
+      return;
+    }
+    if (isBoardFull()) {
+      endGame("DRAW");
+      return;
+    }
+    updateHeaderFooter("Player 1 Turn", xImageLink);
+  } else {
+    turnO(ticTacToeItem);
+    if (checkWin("O")) {
+      endGame("O");
+      return;
+    }
+    if (isBoardFull()) {
+      endGame("DRAW");
+      return;
+    }
+    updateHeaderFooter("Player 2 Turn", oImageLink);
+  }
+  toggleTurn();
+}
+
+function turnO(ticTacToeItem) {
+  updateTicTacToeItem(ticTacToeItem, oImageLink, "O");
+}
+
+function turnX(ticTacToeItem) {
+  updateTicTacToeItem(ticTacToeItem, xImageLink, "X");
+}
+
+function updateTicTacToeItem(ticTacToeItem, imgLink, player) {
+  ticTacToeItem.innerHTML = `<img src="${imgLink}" alt="Img" class="tic-tac-toe-item-img" />`;
+  ticTacToeItem.setAttribute("data-used", player);
+}
+
+function attachTicTacToeEventListeners() {
+  ticTacToe.addEventListener("click", (e) => {
+    const target = e.target.closest(".tic-tac-toe-item");
+    if (target) ticTacToeItemHandler(target);
+  });
+}
+
+function showPauseModal() {
+  toggleModalVisibility(false);
+}
+
+function hidePauseModal() {
+  toggleModalVisibility(true);
+}
+
+function toggleModalVisibility(isHidden) {
+  pauseModal.classList.toggle("d-none", isHidden);
+  overlayPause.classList.toggle("d-none", isHidden);
+}
+
+function attachNavigationListeners() {
+  [backIcon, backBtn].forEach((btn) => {
+    btn.addEventListener("click", () => (window.location.href = "/"));
+  });
+
+  backIcon.addEventListener("dblclick", () => (window.location.href = "/"));
+}
+
+function attachPauseResumeListeners() {
+  pauseIcon.addEventListener("click", showPauseModal);
+  resumeBtn.addEventListener("click", hidePauseModal);
+}
+
+function isBoardFull() {
+  const board = Array.from(ticTacToe.children).map((cell) =>
+    cell.getAttribute("data-used")
+  );
+  return board.every((cell) => cell !== "NONE");
+}
+
+function checkWin(player) {
+  const board = Array.from(ticTacToe.children).map((cell) =>
+    cell.getAttribute("data-used")
+  );
+
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  return winningCombinations.some((combination) =>
+    combination.every((index) => board[index] === player)
+  );
+}
+
+function endGame(result) {
+  isGameOver = true;
+  if (result === "DRAW") {
+    headerFooterText.textContent = "It's a Draw!";
+    headerFooterImgLink.src = "../images/draw.png ";
+    headerFooterImgLink.style = "width: 50px; height: 50px;";
+  } else {
+    headerFooterText.textContent =
+      result === "X" ? "Player 1 Wins!" : "Player 2 Wins!";
+  }
+}
